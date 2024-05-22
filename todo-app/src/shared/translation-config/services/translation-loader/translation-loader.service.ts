323367c80620528b223b96307a3ef14265acfe3e
WeszeRita <weszelovszkirita@gmail.com>
2024-05-22 10:59:14 +0200

    add alias to shared module

--- todo-app/src/shared/translation-config/services/translation-loader/translation-loader.service.ts
+++ todo-app/src/shared/translation-config/services/translation-loader/translation-loader.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { TranslateService } from '@ngx-translate/core';
-import { Language } from 'src/shared/enums';
+import { Language } from '@shared';
 
 @Injectable()
 export class TranslationLoaderService {
