9463c07383b71c07bcf87098f896909aae249e73
WeszeRita <weszelovszkirita@gmail.com>
2024-06-06 10:58:09 +0200

    add CVA to searchInput

--- src/shared/components/search/search.component.ts
+++ src/shared/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, DestroyRef, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
 import { TodoFacadeService } from '@shared';
@@ -9,9 +9,20 @@ import { TodoFacadeService } from '@shared';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: SearchComponent,
+      multi: true,
+    },
+  ],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, ControlValueAccessor {
+  value: string;
   searchTermInput = new FormControl(null);
+
+  private onChange: (value: string) => void = () => {};
+  protected onTouched: () => void = () => {};
 
   constructor(private destroyRef: DestroyRef, private todoFacadeService: TodoFacadeService) {}
 
@@ -22,7 +33,24 @@ export class SearchComponent implements OnInit {
         distinctUntilChanged(),
         takeUntilDestroyed(this.destroyRef),
       )
-      .subscribe((value: string) => this.todoFacadeService.searchTodo(value));
+      .subscribe(() => this.todoFacadeService.searchTodo(this.value));
+  }
+
+  setValue(input: Event): void {
+    this.writeValue((input.target as HTMLInputElement).value);
+  }
+
+  writeValue(value: string): void {
+    this.value = value;
+    this.onChange(this.value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 
   buildTranslationKey(relativeKey: string): string {
